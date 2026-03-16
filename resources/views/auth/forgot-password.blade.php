51683cbe213ce8b6fe38315b6060b7c9c46c5936
Joao Felipe <joaofelipeaires123@gmail.com>
2026-03-16 13:48:18 -0300

    [ui]tradução das paginas de login

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        Esqueceu sua senha? Sem problema. Basta nos informar seu endereço de e-mail e enviaremos um link de redefinição de senha que permitirá escolher uma nova.
     </div>
 
     <!-- Session Status -->
@@ -9,16 +9,15 @@
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="'E-mail'" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                Link de Reset de Senha de Email
             </x-primary-button>
         </div>
     </form>
